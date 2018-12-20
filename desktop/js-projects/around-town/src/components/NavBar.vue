98e942d58757fbb59308bab5c28c2c495910b8a0
Michael Hicks <kustomdesigner@gmail.com>
2018-12-19 23:26:05 -0500

    Finished Nav Styling and added Bootstrap-Vue

---
+++ desktop/js-projects/around-town/src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-container class="bv-example-row">
+        <b-row>
+            <b-navbar-brand href="/"><img width="60%" src="../assets/logo.svg" alt="logo" /></b-navbar-brand>
+            <b-navbar>
+                <b-nav class="ml-auto">
+                  <b-nav-item><router-link to="/">Home</router-link></b-nav-item>
+                  <b-nav-item><router-link to="/about">About</router-link></b-nav-item>
+                </b-nav>
+            </b-navbar>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+    li.nav-item {
+        list-style: none;
+    }
+    .navbar {
+        margin-left: auto;
+    }    
+</style>
---
+++ desktop/js-projects/around-town/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>How it works</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
